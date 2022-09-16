c3f041986df8c7bb3dcfc0f74ca73550b42f9a5c
XCR90 <yuratimes@gmail.com>
2022-09-16 08:35:53 +0700

    ganti template

---
+++ assets/style.js
@@ -0,0 +1,5 @@
+// let elements = document.querySelectorAll('aside a');
+// console.log(elements);
+// elements.forEach((value, key) => {
+//     value.style['animation-delay'] = key * .3 + 's'
+// })
